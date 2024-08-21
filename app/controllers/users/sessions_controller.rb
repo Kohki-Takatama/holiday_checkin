807aa523ea702a51231e46fb498e1841fcd882c8
Kohki-Takatama <koh.takatama@gmail.com>
2024-08-21 18:08:04 +0900

    add:ログイン後の表示ページ設定メソッドを追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -21,12 +21,18 @@ class Users::SessionsController < Devise::SessionsController
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
+
+  # tama add after_sign_in_path_method
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 
   # tama add after_sign_out_path_method
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
+  # up-to-here
 end
